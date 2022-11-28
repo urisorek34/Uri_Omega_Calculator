91f827e1115f1e286ce789ebeb7ca4b8c9d354f4
urisorek <urisorek34@gmail.com>
2022-11-29 01:42:10 +0200

    Add to config file the new operand and priority dict
    priority of # is 6

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ Description: configuration file for the calculator.
 BINARY_OPERATORS_LIST = ["-", "+", "*", "/", "^", "&", "$", "@"]
 UNARY_OPERATORS_LIST_LEFT = ["~", "#"]
 UNARY_OPERATORS_LIST_RIGHT = ["!"]
+PRIORITY_DICT = {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3, "%": 4, "$": 5, "&": 5, "@": 5, "!": 6, "~": 6,"#": 6}
