024886a3d92fd03e3243134d2e67db038f972dc7
urisorek <urisorek34@gmail.com>
2022-11-28 13:58:24 +0200

    add func --> Check if there is a space between two numbers
    add function check_space_between_numbers.

--- equation_validation.py
+++ equation_validation.py
@@ -2,5 +2,30 @@
 Auther: Uri Sorek
 Date:
 
-Description: this module contains formula validation check for the input string (what to calculate).
+Description: this module contains formula validation check for the input string.
 """
+
+
+def check_equation_validation(equation: str) -> bool:
+    """
+    This method checks the validation of the equation given from the user.
+    :param: equation: the equation.
+    :return: True if the equation is valid, False otherwise.
+    """
+    pass
+
+
+def check_spaces_between_numbers(equation: str) -> bool:
+    """
+    This method checks the spaces between the numbers in the equation.
+    :param: equation: the equation.
+    :return: True if there are spaces and False if not.
+    """
+    current_string_start = 1
+    while space_index != -1:
+        # check for each space if it between two numbers
+        space_index = equation.find(" ", current_string_start)
+        if equation[space_index - 1].isdigit() and equation[space_index + 1].isdigit():
+            return False
+        current_string_start = space_index + 1
+    return True
