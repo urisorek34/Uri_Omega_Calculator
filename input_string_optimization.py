db8ad9bef645304c353fdabe46c069dbe02be108
urisorek <urisorek34@gmail.com>
2022-12-04 18:19:05 +0200

    fixed_bug in replace unary minus function

--- input_string_optimization.py
+++ input_string_optimization.py
@@ -90,21 +90,23 @@ def replace_minus_with_unary_minus(equation: str) -> str:
     :param equation: the equation string.
     :return: the equation string with unary minus.
     """
-    first_minus_index = equation.find("-")
-    while first_minus_index != -1:
-        if first_minus_index == 0:
+    equation_list = list(equation)
+    minus_index = equation_list.index("-")
+    while minus_index != -1:
+        if minus_index == 0:
             # if the first char is a minus, replace it with unary minus
-            equation = equation.replace("-", "u-", 1)
-        elif equation[first_minus_index - 1] in PRIORITY_DICT.keys():
+            equation_list[minus_index] = "u"
+        elif equation_list[minus_index - 1] in PRIORITY_DICT.keys() or equation_list[minus_index - 1] == "(" or \
+                equation_list[minus_index - 1] == "u":
             # if the minus is after an operator, replace it with unary minus
-            equation = equation.replace("-", "u-", 1)
-        first_minus_index = equation.find("-", first_minus_index + 1)
-    return equation
+            equation_list[minus_index] = "u"
+        minus_index = equation.find("-", minus_index + 1)
+    return "".join(equation_list)
 
 
 def reduce_minuses(equation: str) -> str:
     """
-    return equation with reduced minuses (unary minus).
+    return equation with reduced unary minuses (unary minus).
     :param equation: the given equation string
     :return: the new string with reduced minuses.
     """
