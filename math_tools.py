5dff5fdf1e204bcf486b7a3966217e212ef133d4
urisorek <urisorek34@gmail.com>
2022-12-08 14:09:39 +0200

    Update factorial function

--- math_tools.py
+++ math_tools.py
@@ -7,12 +7,16 @@ Description: this module contains the nontrivial math tools for calculating.
 from math import pow
 
 
-def factorial(num: int) -> int:
+def factorial(num: float) -> float:
     """
     This method calculates the factorial of a number.
     :param num: the number.
     :return: the factorial of the number.
     """
+    if num < 0:
+        raise ValueError("factorial of negative number")  # TODO: add exception
+    if num % 1 != 0:
+        raise ValueError("factorial of float number")  # TODO: add exception
     if num == 0:
         return 1
     return num * factorial(num - 1)
