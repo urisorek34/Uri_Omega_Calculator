8c22ba86d7f19f1f4bc30cbcf5b51b07dd3d5739
urisorek <urisorek34@gmail.com>
2022-12-08 15:30:51 +0200

    add syntax error and math error exceptions classes
    parent exceptions

--- exceptions.py
+++ exceptions.py
@@ -4,3 +4,27 @@ Date:
 
 Description: this module contains the exceptions for the calculator.
 """
+
+
+class SyntaxEquationError(Exception):
+    """
+    This class represents the syntax equation error.
+    """
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return "Syntax Error!"
+
+
+class MathEquationError(Exception):
+    """
+    This class represents the math equation error.
+    """
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return "Math Error!"
