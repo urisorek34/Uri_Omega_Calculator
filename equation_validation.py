63d56907d4c10bd0058b30dcb6859cb333ca5bc6
urisorek <urisorek34@gmail.com>
2022-12-08 18:25:37 +0200

    add MissingBracket exception to equation_validation.py

--- equation_validation.py
+++ equation_validation.py
@@ -5,6 +5,7 @@ Date:
 Description: this module contains formula validation check for the input string (what to calculate).
 """
 from config import OPENER_BRACKET, CLOSER_BRACKET
+from exceptions import MissingBracketError
 
 
 def check_equation_validation(equation: str) -> bool:
@@ -13,27 +14,9 @@ def check_equation_validation(equation: str) -> bool:
     :param: equation: the equation.
     :return: True if the equation is valid, False otherwise.
     """
-    # TODO: exceptions for missing operators and brackets
-    if check_spaces_between_numbers(equation) or not check_brackets_validation(equation):
-        return False
+    if not check_brackets_validation(equation):
+        raise MissingBracketError(equation)
     return True
-
-
-def check_spaces_between_numbers(equation: str) -> bool:
-    """
-    This method checks the spaces between the numbers in the equation.
-    :param: equation: the equation.
-    :return: True if there are spaces and False if not.
-    """
-    current_string_start = 1
-    space_index = 0
-    while space_index != -1:
-        # check for each space if it between two numbers
-        space_index = equation.find(" ", current_string_start)
-        if equation[space_index - 1].isdigit() and equation[space_index + 1].isdigit():
-            return True
-        current_string_start = space_index + 1
-    return False
 
 
 def check_brackets_validation(equation: str) -> bool:
