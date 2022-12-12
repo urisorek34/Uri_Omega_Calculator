ee191c8a2d1203f357db6356f544441374e88159
urisorek <urisorek34@gmail.com>
2022-12-12 16:54:22 +0200

    nonsense string and empty string testing

--- pytest_for_calculator.py
+++ pytest_for_calculator.py
@@ -10,7 +10,7 @@ from user_communication import get_result_with_exception_handling
 
 def test_syntax_error():
     """
-    This method tests the main function.
+    This method tests syntax errors.
     :return: None.
     """
     testing_list = ["3*^2", ")7*3", "3*3(", "3*3)", "3*3+4-5#2"]
@@ -19,4 +19,17 @@ def test_syntax_error():
         assert "Syntax Error!\n" in get_result_with_exception_handling(test)
 
 
-test_syntax_error()
+def test_non_sense():
+    """
+    This method tests nonsense input.
+    :return: None.
+    """
+    assert "Syntax Error!\n" in get_result_with_exception_handling("dfsdfafdsg3241-=-sdfg")
+
+
+def test_empty_input():
+    """
+    This method tests empty input.
+    :return: None.
+    """
+    assert "Syntax Error!\n" in get_result_with_exception_handling("")
